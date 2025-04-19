2cf5cf242ee93ee7b0ea5f4bcb7709f0910becfe
Max Derbenwick <maximusderby@gmail.com>
2025-04-19 11:30:17 +0200

    Miscellaneous old changes

--- public/nodes.js
+++ public/nodes.js
@@ -1,22 +1,38 @@
 
-const minNodeDistance = 11;
-const maxNodeDistance = 15;
-const radius = 3;
-const lineWidth = 0.4;
+const minNodeDistance = 0;
+const maxNodeDistance = 18;
+const maxLineWidth = 0.8;
+const maxInnerRadius = 4;
+const maxOuterRadius = 20;
+const maxAngFreq = 0.2;
 
 const distance = (node0, node1) => Math.sqrt((node1.x - node0.x)**2 + (node1.y - node0.y)**2);
+const rgbString = col => `rgb(${Math.floor(col.r * 255)}, ${Math.floor(col.g * 255)}, ${Math.floor(col.b * 255)})`;
 
 export function drawNodes(image, nodes) {
+   image.context.rect(0, 0, image.getWidth(), image.getHeight());
+   image.context.fillStyle = '#222';
+   image.context.fill();
+
    for(const node of nodes) {
+      image.context.fillStyle = rgbString(node.color);
       image.context.beginPath();
-      image.context.arc(node.x, node.y, radius, 0, 2 * Math.PI, false);
+      image.context.arc(node.x, node.y, node.innerRadius, 0, 2 * Math.PI, false);
       image.context.fill();
    }
 
-   image.context.lineWidth = lineWidth;
    for(const node0 of nodes) {
       for(const node1 of nodes) {
-         if(distance(node0, node1) <= maxNodeDistance && distance(node0, node1) >= minNodeDistance) {
+         let dist = distance(node0, node1);
+
+         if(dist <= maxNodeDistance && dist >= minNodeDistance) {
+            let gradient = image.context.createLinearGradient(node0.x, node0.y, node1.x, node1.y);
+            gradient.addColorStop(0, rgbString(node0.color));
+            gradient.addColorStop(1, rgbString(node1.color));
+            image.context.strokeStyle = gradient;
+
+            image.context.lineWidth = (1 - (dist - minNodeDistance) / (maxNodeDistance - minNodeDistance)) * maxLineWidth;
+
             image.context.beginPath();
             image.context.moveTo(node0.x, node0.y);
             image.context.lineTo(node1.x, node1.y);
@@ -35,11 +51,28 @@ export function generateNodes(minX, minY, maxX, maxY, density) {
    let numNodes = density * area;
    for(let i = 0; i < numNodes; ++i) {
       nodes.push({
-         x: Math.random() * (maxX - minX) + minX,
-         y: Math.random() * (maxY - minY) + minY
+         center: {
+            x: Math.random() * (maxX - minX) + minX,
+            y: Math.random() * (maxY - minY) + minY
+         },
+         innerRadius: Math.random() * maxInnerRadius,
+         outerRadius: Math.random() * maxOuterRadius,
+         angFreq: (Math.random() * 2 - 1) * maxAngFreq,
+         color: {
+            r: Math.random() * 0.2,
+            g: Math.random(),
+            b: Math.random() * 0.6
+         }
       });
    }
 
    return nodes;
+}
+
+export function animateNodes(nodes, time) {
+   for(let node of nodes) {
+      node.x = node.center.x + node.outerRadius * Math.cos(time * 2 * Math.PI * node.angFreq);
+      node.y = node.center.y + node.outerRadius * Math.sin(time * 2 * Math.PI * node.angFreq);
+   }
 }
 
--- public/script.js
+++ public/script.js
@@ -1,32 +1,36 @@
 import Image from './image.js';
 import { perlin, whiteStatic, rgbStatic } from './noise.js';
-import { generateNodes, drawNodes } from './nodes.js';
+import { generateNodes, drawNodes, animateNodes } from './nodes.js';
 
-function drawStereogram(noise, distort, dest) {
-   for(let x = 0; x < dest.getWidth(); ++x) {
-      for(let y = 0; y < dest.getHeight(); ++y) {
-         let distortion = 0;
-         if(distort)
-            distortion = distort.sample(dest.textureCoords(x, y)).r;
+function calculateDistortion(nodes, map, image) {
+   for(let node of nodes) {
+      let distortion = 0;
+      if(map)
+         distortion = map.sample(image.textureCoords(node.x, node.y)).r;
 
-         distortion *= 8; // intensity
-         dest.setPixel(x, y, noise(x + Math.floor(distortion), y));
-      }
+      node.x -= (distortion * 2 - 1) * 8;
    }
 }
 
 const left = Image.fromDocument(document.getElementById('left'));
 const right = Image.fromDocument(document.getElementById('right'));
+const nodes = generateNodes(-40, -40, left.getWidth() + 40, left.getHeight() + 40, 0.01);
 
-const nodesL = generateNodes(0, 0, left.getWidth(), left.getHeight(), 0.015);
-const nodesR = [];
-for(let node of nodesL) {
-   if(node.x >= 50 && node.x <= 150 && node.y >= 50 && node.y <= 150)
-      nodesR.push({ x: node.x - 5, y: node.y });
-   else
-      nodesR.push({ x: node.x, y: node.y });
+let dudv;
+
+function tick(time) {
+   time /= 1000; // ms to s
+
+   animateNodes(nodes, time);
+   drawNodes(left, nodes);
+   calculateDistortion(nodes, dudv, right);
+   drawNodes(right, nodes);
+   
+   requestAnimationFrame(tick);
 }
 
-drawNodes(left, nodesL);
-drawNodes(right, nodesR);
+Image.fromFile('/dudv.png').then(result => {
+   dudv = result;
+   requestAnimationFrame(tick);
+});
 
